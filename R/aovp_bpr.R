5fabf5a0567154f1180ce1c62d3de7ec3a9dcfd2
barneyricca <barneyricca@gmail.com>
2025-11-09 11:53:51 -0500

    First function

---
+++ R/aovp_bpr.R
@@ -0,0 +1,18 @@
+#' Wrap around lmPerm::aovp()
+#'
+#' @param formula A valid R formula
+#' @param data Data (with at least two columns)
+#'
+#' @return An aovp object
+#' @export
+
+aovp_bpr <- function(formula,
+                     data = NULL,
+                     perm = "Exact") {
+  # This function is simply a wrapper around lmPerm::aovp(), and so it is
+  #  superfluous. It was written as a way to establish the package for DSCC 487,
+  #  and does NOT include validation or other error trapping.
+  return(
+    aovp(formula,
+         data))
+}
